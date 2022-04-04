f439bbf8012c708c2739e98373234f8e47b2099f
Abhiraj Mohan <abhirajmohan@infra04-wg012.eduroam.wireless.umn.edu>
2022-04-04 17:58:49 -0500

    adding curated tables

---
+++ create_curated_tables.sql
@@ -0,0 +1,28 @@
+-- environment set-up
+use role sysadmin;
+use warehouse compute_wh;
+
+-- use the old schema
+use database preql_sales;
+use schema raw;
+
+-- clone the raw schema
+create schema curated clone raw;
+
+-- switch to the newly created schema
+use database preql_sales;
+use schema curated;
+
+-- delete old data from tables
+
+delete from customers;
+delete from employees;
+delete from products;
+delete from sales;
+
+-- populate tables with new data
+
+insert into customers (select distinct * from preql_sales.raw.customers);
+insert into employees (select distinct * from preql_sales.raw.employees);
+insert into products (select distinct * from preql_sales.raw.products);
+insert into sales (select distinct * from preql_sales.raw.sales);
